2ef3b6eff7da856d9c561344f6ae6bdf8e63b453
David Weichert <info@davidweichert.de>
2015-11-08 10:47:49 +0100

    Fix unit tests to reflect wording changes.

--- src/EmperorNortonCommands/tests/lib/DdateLocalizedTest.php
+++ src/EmperorNortonCommands/tests/lib/DdateLocalizedTest.php
@@ -42,19 +42,19 @@ class DdateLocalizedTest extends \PHPUnit_Framework_TestCase
         return array(
             array('03051998', 'Pungenday, Discord 50, 3164 YOLD', null, 'en'),
             array('29021996', 'St. Tibs Tag 3162 n. Gre.', null, 'de'),
-            array('07021974', 'Stechend, Verwirrung 38, 3140 n. Gre.', null, 'de'),
+            array('07021974', 'Stichtag, Verwirrung 38, 3140 n. Gre.', null, 'de'),
             array('03012013', 'ST, Verwirrung 3, 3179', '%{%a, %B %d,%} %Y', 'de'),
-            array('08122014', 'Heute ist Dröhntag, der Fünfzigster Ausklang 3180 n. Gre.' . "\n\t" . 'Wir feiern Ausfluss.', 'Heute ist %{%A, der %E %B %Y n. Gre.%N%n%tWir feiern %H.', 'de'),
+            array('08122014', 'Heute ist Blütezeit, der Fünfzigster Grummet 3180 n. Gre.' . "\n\t" . 'Wir feiern Ausfluss.', 'Heute ist %{%A, der %E %B %Y n. Gre.%N%n%tWir feiern %H.', 'de'),
             array('14031999', 'ST, Ve Dreiundsiebzigster 3165', '%a, %b %E %Y', 'de'),
             array('29022012', "Heute ist St. Tibs Tag 3178, noch 2428624 Tage bis zum Tag X", "Heute ist %{%A, the %E of %B,%} %Y, noch %X Tage bis zum Tag X", 'de'),
             array('28022016', 'Heute ist Prickel-Prickel, Neunundfünfzigster d. Verwirrung im JUHD 3182.', 'Heute ist %{%A, %E d. %B%} im JUHD %Y.%N Wir feiern %H.', 'de'),
             array('28022016', 'Heute ist Prickel-Prickel, Neunundfünfzigster der Verwirrung im JUHD 3182.', 'Heute ist %{%A, %E %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
-            array('05122015', 'Heute ist Prickel-Prickel, Siebenundvierzigster des Ausklangs im JUHD 3181.', 'Heute ist %{%A, %E %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
-            array('08122015', 'Heute ist Dröhntag, Fünfzigster des Ausklangs im JUHD 3181. Wir feiern Ausfluss.', 'Heute ist %{%A, %E %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
-            array('05122015', 'Heute ist Prickel-Prickel, 47. des Ausklangs im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
-            array('11112015', 'Heute ist Orangewerdend, 23. des Ausklangs im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
+            array('05122015', 'Heute ist Prickel-Prickel, Siebenundvierzigster des Grummets im JUHD 3181.', 'Heute ist %{%A, %E %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
+            array('08122015', 'Heute ist Blütezeit, Fünfzigster des Grummets im JUHD 3181. Wir feiern Ausfluss.', 'Heute ist %{%A, %E %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
+            array('05122015', 'Heute ist Prickel-Prickel, 47. des Grummets im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
+            array('11112015', 'Heute ist Orangewerdend, 23. des Grummets im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
             array('26092015', 'Heute ist Prickel-Prickel, 50. der Beamtenherrschaft im JUHD 3181. Wir feiern Beamtenfluss.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
-            array('20102015', 'Heute ist Stechend, 1. des Ausklangs im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
+            array('20102015', 'Heute ist Stichtag, 1. des Grummets im JUHD 3181.', 'Heute ist %{%A, %e %C%} im JUHD %Y.%N Wir feiern %H.', 'de'),
         );
     }
 
